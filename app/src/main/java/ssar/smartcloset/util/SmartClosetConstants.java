018ba6686e2c86a54b63729318ef3b8419455def
Sadaf Syed <sh.sadaf@gmail.com>
2014-11-24 04:58:19 -0600

    merged latest from newtag branch

--- app/src/main/java/ssar/smartcloset/util/SmartClosetConstants.java
+++ app/src/main/java/ssar/smartcloset/util/SmartClosetConstants.java
@@ -10,7 +10,7 @@ public class SmartClosetConstants {
     public static final String MAINMENU_SEARCH_BUTTON = "MAINMENU_SEARCH_BUTTON";
     public static final String MAINMENU_NEWTAG_BUTTON = "MAINMENU_NEWTAG_BUTTON";
 
-    public static final String APP_SPOT = "http://storefrontssar2.appspot.com";
+    public static final String APP_SPOT = "http://data-concord-766.appspot.com";
     public static final String CREATE_ARTICLE = APP_SPOT + "/CreateArticle";
     public static final String UPLOAD_ARTICLE_IMAGE = APP_SPOT + "/AndroidUploadHandler";
 
